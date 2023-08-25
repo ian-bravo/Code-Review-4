09d27ec68d8375bf7ca7315b183bd6576e988238
ian-bravo <bravo.ian@gmail.com>
2023-08-25 10:34:25 -0700

    pass Test 1 for Pizza.prototype.costForSize

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,17 @@ Pizza.prototype.costForToppings = function () {
   this.toppings.length * 4;
   console.log(this.toppings.length * 4);
 };
+
+Pizza.prototype.costForSize = function () {
+  switch (this.size) {
+    case ("large"):
+      console.log("large cost +5");
+      break;
+    case ("medium"):
+      console.log("medium cost +4");
+      break;
+    case ("small"):
+      console.log("small cost +3"); 
+      break;
+  }
+}
