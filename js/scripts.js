47532b09e84c635b7d0a5ec648aa04ad2bba192b
ian-bravo <bravo.ian@gmail.com>
2023-08-25 19:06:10 -0700

    refactor UI and HTML

--- js/scripts.js
+++ js/scripts.js
@@ -12,19 +12,19 @@ Pizza.prototype.costForToppings = function () {
 Pizza.prototype.costForSize = function () {
   switch (this.size) {
     case ("large"):
-      return 5;
+      return 9;
       break;
     case ("medium"):
-      return 4;
+      return 7;
       break;
     case ("small"):
-      return 3; 
+      return 5; 
       break;
   }
 }
 
 Pizza.prototype.totalCost = function () {
-  let totalCost = (this.costForSize() + this.costForToppings()) +5;
+  let totalCost = (this.costForSize() + this.costForToppings());
   return totalCost;
 }
 
@@ -37,13 +37,33 @@ Pizza.prototype.totalCost = function () {
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const toppingChoices = document.querySelector("input[name='toppings']:checked").value
+    const toppingChoices = document.querySelector("input[name='toppings']:checked").value;
+
+//NEED TO FIND HOW TO GET CHECKBOX VALUES - watch youtube
+
+  // let toppingChoices = document.getElementsByName('toppings').forEach(function(checked){
+  //   checked.addEventListener('click', function () {
+  //     if(this.checked){
+  //       // return this.value;
+  //       console.log(this.value);
+  //     }
+  //   });
+  // });
+
+  // let checkedToppings = document.querySelectorAll('.toppings')
+  //   checkedToppings.forEach((checkbox))
+  console.log(toppingChoices);
   const sizeChoice = document.querySelector("input[name='size']:checked").value;
+  console.log(sizeChoice);
   let orderPizza = new Pizza(toppingChoices, sizeChoice);
-  orderPizza.totalCost(); 
+  
+  const displayTotal = orderPizza.totalCost(); 
+  console.log(displayTotal);
+  document.querySelector("span#display-total").innerText = displayTotal;
+  
 }
 
 
-window.addEventListener("load", function) () {
+window.addEventListener("load", function () {
   document.querySelector("form#order-form").addEventListener("submit", handleFormSubmission)
-}
+});
